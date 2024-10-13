4b2706523dcc88ac3c91de0ed7bc204c19500366
igorxciv <icheliadinski@gmail.com>
2024-10-12 20:34:38 -0600

    feat: add db connection

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:7878").expect("failed to bind port");
+    let configuration = get_configuration().expect("failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(&address).expect("failed to bind port");
     run(listener)?.await
 }
